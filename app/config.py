9aea7a3e94097c30e510a9828e921bb08de0ec80
qelio <slava.samokhvalov@mail.ru>
2025-12-16 02:48:59 +0300

    update_for_app_platform

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://cabinet:}IhoLy<4!<dL23@192.168.0.4:3306/speechtherapistsoffice'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
 
